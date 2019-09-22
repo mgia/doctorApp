25db8d0017fa97a6e7d0628b32367847b74b1a12
Marc G Tan <findmarcg@gmail.com>
2019-09-22 18:58:33 +0800

    Added homepage navigation

--- App.js
+++ App.js
@@ -1,13 +1,72 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Doctor Profile"
+          onPress={() => this.props.navigation.navigate('Doctor')}
+        />
+        <Button
+          title="Consult Doctor"
+          onPress={() => this.props.navigation.navigate('Chat')}
+        />
+        <Button
+          title="App Guidelines"
+          onPress={() => this.props.navigation.navigate('Intro')}
+        />
+      </View>
+    )
+  }
 }
+
+class DoctorScreen extends Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text>Doctor Profile Screen</Text>
+      </View>
+    )
+  }
+}
+
+class ChatScreen extends Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text>Chat Screen</Text>
+      </View>
+    )
+  }
+}
+
+class IntroScreen extends Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text>App Usage Screen</Text>
+      </View>
+    )
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Doctor: DoctorScreen,
+    Chat: ChatScreen,
+    Intro: IntroScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -16,3 +75,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   }
 })
+
+export default createAppContainer(AppNavigator);
