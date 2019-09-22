501f206904b82d63f83b858b93873e8d87f2a0d4
Marc G Tan <findmarcg@gmail.com>
2019-09-22 19:43:42 +0800

    Added bottom tabs navigation

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import { Typography, Colors } from './styles'
 
@@ -80,9 +82,51 @@ class IntroScreen extends Component {
   }
 }
 
+class SettingsScreen extends Component {
+  static navigationOptions = {
+    title: 'Settings',
+  };
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Text>Settings Screen</Text>
+      </View>
+    )
+  }
+}
+
+const HomeNavigator = createBottomTabNavigator({
+    Home: {
+        screen: HomeScreen,
+        navigationOptions: {
+            title: "Home",
+            tabBarIcon: () => (
+                <Icon
+                    name="user-circle"
+                    size={17}
+                />
+            )
+        }
+    },
+    Settings: {
+        screen: SettingsScreen,
+        navigationOptions: {
+            tabBarLabel: "Settings",
+            tabBarIcon: () => (
+                <Icon
+                    name="cog"
+                    size={17}
+                />
+            )
+        }
+    }
+});
+
+
 const AppNavigator = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home: HomeNavigator,
     Doctor: DoctorScreen,
     Chat: ChatScreen,
     Intro: IntroScreen,
@@ -91,6 +135,7 @@ const AppNavigator = createStackNavigator(
     initialRouteName: 'Home',
   }
 );
+
 
 const styles = StyleSheet.create({
   container: {
